651ac931a0dfcff351f73a1bec5a6d04377bbd8a
Jem777 <Clifford_Burton@web.de>
2010-04-30 18:36:32 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='rattlekekz-plugins',
       author="rattlekekz Team",
       author_email="egg@spam.de",
       packages=['rattlekekz', 'rattlekekz.plugins'],
-      scripts=['bin/blinklight'],
+      #scripts=['bin/blinklight'],
       requires=['twisted(>=8.1.0)','urwid','simplejson','OpenSSL'],
       url="http://kekz.net/",
       license="GPL v3 or higher"
